5ea769b96fa5f8d41869c7b61dd421a9f23ec009
RaygunsSA <52988159+RaygunsSA@users.noreply.github.com>
2020-05-31 23:08:59 +0200

    Add files via upload

---
+++ crime_list_screen.dart
@@ -0,0 +1,76 @@
+//import 'package:crimap_flutter/screens/disclose_crime_screen.dart';
+import 'package:crimap_flutter/models/place.dart';
+import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'package:provider/provider.dart';
+
+import '../services/auth.dart';
+import '../providers/list_crime_locations.dart';
+import './crime_detail_screen.dart';
+
+class CrimeListScreen extends StatelessWidget {
+
+  final AuthService _auth = AuthService();
+
+  @override
+  Widget build(BuildContext context) {
+    ChangeNotifierProvider<CrimeLocation>(
+      create: (context)=> CrimeLocation(),
+      child: Consumer<CrimeListScreen>(
+        builder:(context,provider,child
+      ) =>Scaffold(
+      backgroundColor: Colors.blue[100],     
+      appBar: AppBar(
+        backgroundColor: Colors.blue[400],
+        elevation: 0.0,
+        title: Text('Crime List Screen'),
+        actions: <Widget>[
+          FlatButton.icon(
+             icon: Icon(Icons.add),
+              label: Text('Logout'),
+               onPressed: () async{
+                 await _auth.signOut();
+               },
+          )
+        ],
+      ),
+       body: FutureBuilder( 
+          future: Provider.of<CrimeLocation>(context)
+          .fetchAndSetPlaces(),         
+         builder: (ctx, snapshot) => snapshot.connectionState ==
+           ConnectionState.waiting 
+           ? Center (
+              child: CircularProgressIndicator(),
+             )
+             :Consumer<CrimeLocation>(
+             child: Center(
+               child: Text('No Crime Locations reported yet'),
+             ) ,
+             builder:(ctx, crimeLocations, ch) =>
+             crimeLocations.items.length <= 0
+              ? ch: 
+             ListView.builder(
+                itemCount: crimeLocations.items.length,
+                itemBuilder: (ctx,i) => ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: FileImage(
+                       crimeLocations.items[i].image,
+                        ),
+                      ),
+                  title: Text(crimeLocations.items[i].title),  
+                  subtitle: Text(crimeLocations.items[i].location.address),
+                  onTap: (){
+                     Navigator.of(context).pushNamed(
+                       CrimeDetailScreen.routeName,
+                       arguments: crimeLocations.items[i].id,
+                       );
+                     },
+                  ),
+                ),
+              ),
+        ), 
+       ),
+      ),
+    );              
+  }
+}
